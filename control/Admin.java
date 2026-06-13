68a02a22da12e712f2ae8f78fbbe05af263ef687
Muhammad Umar Rusyad <umarrusyad@gmail.com>
2026-06-13 22:16:25 +0800

    first commit

---
+++ control/Admin.java
@@ -0,0 +1,69 @@
+package control;
+import structure.Building;
+import structure.Room;
+import EnergyController;
+import core.Appliance;
+
+
+
+
+public class Admin {
+    private String username;
+    private String password;
+    private Building managedBuilding;
+
+    public Admin(String u, String p) {
+        username = u;
+        password = p;
+    }
+
+    public String adminGetUsername() {
+        return username;
+    }
+
+    public String adminGetPassword() {
+        return password;
+    }
+
+    public Room viewRooms() {
+        System.out.println("\n========== BUILDING STATUS ==========");
+        System.out.println("Building : " + managedBuilding.getBuildingName());
+
+        for (Room room : managedBuilding.getRoomName()) {
+            System.out.println("Room: " + room.getRoomName());
+            for (Appliance a : room.getAppliances()) {
+                System.out.printf("%-20s | Status: %-4s | Energy: %.2f kWh%n", a.getApplianceName());
+            }
+        }
+    }
+
+    public boolean loginAdmin(String u, String p) {
+        return (this.username == u && this.password == p);
+    }
+
+    public void turnOnDevice() {
+        Appliance a = managedBuilding.getApplianceName(roomName, applianceName);
+        a.turnOn();
+        System.out.println("<ADMIN> " + applianceName + " in " + roomName + " is turned ON.");
+    }
+
+    public void turnOffDevice() {
+        Appliance a = managedBuilding.getApplianceName(roomName, applianceName);
+        a.turnOff();
+        System.out.println("<ADMIN> " + applianceName + " in " + roomName + " is turned OFF.");
+    }
+
+    public void generateReport(EnergyController e) {
+        System.out.println("\n===== ENERGY USAGE SUMMARY =====");
+        System.out.println("Admin    : " + username + "  (ID: " + password + ")");
+        e.printEnergyReport(managedBuilding);
+        System.out.println("================================\n");
+    }
+
+    @Override
+    public String toString() {
+        return ("------------ SYSTEMS REPORT ------------");
+
+    }
+
+}
---
+++ control/Main.java
@@ -0,0 +1,25 @@
+package control;
+import core.AirConditioner;
+import core.Fan;
+import core.Light;
+import io.EnergyReport;
+import io.FileManager;
+import structure.Building;
+import structure.DeviceNotFoundException;
+import structure.Room;
+
+public class Main {
+    public static void main(String[] args) {
+
+    System.out.println("═════════════════════════════════════════");
+    System.out.println("   Smart Building Energy Mgmt System     ");
+    System.out.println("   Green Tower — UTM Campus              ");
+    System.out.println("═════════════════════════════════════════");
+    System.out.println();
+
+
+    Admin admin = new Admin("Admin", "12345");
+
+
+    }
+}
