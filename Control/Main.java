6281558bf51d67097f0336de0069984c242e40f0
Muhammad Umar Rusyad <umarrusyad@gmail.com>
2026-06-15 22:42:03 +0800

    save my changes

--- Control/Main.java
+++ Control/Main.java
@@ -1,15 +1,25 @@
-package Control;
+package control;
+import core.AirConditioner;
+import core.Fan;
+import core.Light;
+import io.EnergyReport;
+import io.FileManager;
+import structure.Building;
+import structure.DeviceNotFoundException;
+import structure.Room;
 
 public class Main {
-    /*
-     * Things need:
-     * 
-     * 1. Admin login (password)
-     * 2. Add/remove area
-     * 3. Add/remove sensor/appliance in area.
-     * 4. Admin able to control area's appliance (turn on/off).
-     * 5. receive alert that will navigate to the correct area.
-     * 6. A page that can see the appliance threshold and change it.
-     */
+    public static void main(String[] args) {
 
+    System.out.println("═════════════════════════════════════════");
+    System.out.println("   Smart Building Energy Mgmt System     ");
+    System.out.println("   Green Tower — UTM Campus              ");
+    System.out.println("═════════════════════════════════════════");
+    System.out.println();
+
+
+    Admin admin = new Admin("Admin", "12345");
+
+
+    }
 }
